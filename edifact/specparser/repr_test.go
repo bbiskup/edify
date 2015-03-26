c1075096c7c1bd6a528fb6da8ab45534a1825ca4
Bernhard Biskup <bbiskup@gmx.de>
2015-03-26 20:13:46 +0100

    Added benchmarks for data element repr validation

--- edifact/specparser/repr_test.go
+++ edifact/specparser/repr_test.go
@@ -2,7 +2,9 @@ package specparser
 
 import (
 	sp "edifice/edifact/specparser"
+	"fmt"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -47,10 +49,12 @@ var validationSpec = []struct {
 	{sp.NewRepr(sp.AlphaNum, true, 2), "xx", true},
 	{sp.NewRepr(sp.AlphaNum, true, 3), "x2x", true},
 	{sp.NewRepr(sp.AlphaNum, true, 3), "x2x2", false},
+	{sp.NewRepr(sp.AlphaNum, true, 3), "", true},
 	{sp.NewRepr(sp.AlphaNum, false, 4), "x2x2", true},
 
 	{sp.NewRepr(sp.Num, true, 3), "123", true},
 	{sp.NewRepr(sp.Num, true, 3), "123a", false},
+	{sp.NewRepr(sp.Num, true, 3), "", true},
 	{sp.NewRepr(sp.Num, false, 3), "123", true},
 	{sp.NewRepr(sp.Num, false, 3), "12", false},
 }
@@ -72,6 +76,37 @@ func TestValidateRepr(t *testing.T) {
 		if !reflect.DeepEqual(res, spec.expected) {
 			t.Fatalf("Test string: %s: expected: %#v, got: %#v",
 				spec.testStr, spec.expected, res)
+		}
+	}
+}
+
+func BenchmarkValidateShortExpr(b *testing.B) {
+	repr, err := sp.ParseRepr("an..10")
+	if err != nil {
+		b.Fatalf("Parse error: %s", err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		res, err := repr.Validate("abcdef1234")
+		if !res {
+			b.Fatalf(fmt.Sprintf("Validation failed: %s", err))
+		}
+	}
+}
+
+func BenchmarkValidateLongExpr(b *testing.B) {
+	repr, err := sp.ParseRepr("an..10000")
+	if err != nil {
+		b.Fatalf("Parse error: %s", err)
+	}
+
+	testStr := strings.Repeat("x", 10000)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		res, err := repr.Validate(testStr)
+		if !res {
+			b.Fatalf(fmt.Sprintf("Validation failed: %s", err))
 		}
 	}
 }
