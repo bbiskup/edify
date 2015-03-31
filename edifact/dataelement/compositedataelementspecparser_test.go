8e9a6f881b5d317913a88fb67d4a5736dfc06723
Bernhard Biskup <bbiskup@gmx.de>
2015-03-31 14:45:57 +0200

    New test case

--- edifact/dataelement/compositedataelementspecparser_test.go
+++ edifact/dataelement/compositedataelementspecparser_test.go
@@ -8,7 +8,7 @@ import (
 var parserSpec = []struct {
 	specLines      []string
 	expectedResStr string
-	expectedErr    error
+	expectErr      bool
 }{
 	{
 		[]string{
@@ -17,7 +17,7 @@ var parserSpec = []struct {
 			"       5393  Price multiplier type code qualifier      C      an..3",
 		},
 		"Composite C138 PRICE MULTIPLIER INFORMATION 1 (conditional)\n\tComponent 5394 (mandatory)\n\tComponent 5393 (conditional)",
-		nil,
+		false,
 	},
 	{
 		[]string{
@@ -27,7 +27,16 @@ var parserSpec = []struct {
 			"",
 		},
 		"Composite C138 PRICE MULTIPLIER INFORMATION 2 (mandatory)\n\tComponent 5394 (mandatory)\n\tComponent 5393 (conditional)",
-		nil,
+		false,
+	},
+	{
+		[]string{
+			"020    C138 PRICE MULTIPLIER INFORMATION               X    2      ",
+			"       5394  Price multiplier rate                     M      n..12",
+			"",
+		},
+		"",
+		true,
 	},
 }
 
@@ -35,7 +44,7 @@ func TestParser(t *testing.T) {
 	for _, spec := range parserSpec {
 		parser := NewCompositeDataElementSpecParser()
 		res, err := parser.Parse(spec.specLines)
-		if err != nil && spec.expectedErr != nil {
+		if err != nil && spec.expectErr {
 			continue
 		}
 
