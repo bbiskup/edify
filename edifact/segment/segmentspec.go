3284600ad94387e9d70f4082ea20c93c4e518eb6
Bernhard Biskup <bbiskup@gmx.de>
2015-03-31 18:50:18 +0200

    Partial segment spec

--- edifact/segment/segmentspec.go
+++ edifact/segment/segmentspec.go
@@ -1,6 +1,18 @@
 package segment
 
+import (
+	"fmt"
+)
+
 // Segment specification
 type SegmentSpec struct {
-	Name string
+	Name                string
+	Function            string
+	DataElementSpecNums []int32
+}
+
+func (s *SegmentSpec) String() string {
+	return fmt.Sprintf(
+		"Segment %s (%d data elems)",
+		s.Name, len(s.DataElementSpecNums))
 }
