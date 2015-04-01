0f4630c41ad65bf0a7eadbceb35c7a5ca108e4a6
Bernhard Biskup <bbiskup@gmx.de>
2015-04-01 16:42:51 +0200

    Moved code for parsing component element spec to segment parser

--- edifact/dataelement/compositedataelementspecparser.go
+++ edifact/dataelement/compositedataelementspecparser.go
@@ -16,46 +16,6 @@ type CompositeDataElementSpecParser struct {
 	componentElemRE *regexp.Regexp
 }
 
-/* TODO: move to segment parser
-// Parse composite element spec header of the form
-// "020    C138 PRICE MULTIPLIER INFORMATION               C    1"
-func (p *CompositeDataElementSpecParser) ParseHeader(header string) (pos int, name string, title string, mandatory bool, count int, err error) {
-	headerMatch := p.headerRE.FindStringSubmatch(header)
-	if headerMatch == nil {
-		err = errors.New(fmt.Sprintf("Missing header section (header: '%s'", header))
-		return
-	}
-
-	if len(headerMatch) != 6 {
-		panic("Internal error: incorrect regular expression")
-	}
-
-	pos, err = strconv.Atoi(headerMatch[1])
-	if err != nil {
-		return
-	}
-
-	name = headerMatch[2]
-	title = strings.TrimSpace(headerMatch[3])
-	mandatoryStr := headerMatch[4]
-
-	if mandatoryStr == "M" {
-		mandatory = true
-	} else if mandatoryStr == "C" {
-		mandatory = false
-	} else {
-		err = errors.New("Neither mandatory nor conditional")
-		return
-	}
-
-	count, err = strconv.Atoi(headerMatch[5])
-	if err != nil {
-		return
-	}
-
-	return
-}*/
-
 /* Parse a line of the form
 010    3148  Communication address identifier          M      an..512
 */
@@ -91,45 +51,6 @@ func (p *CompositeDataElementSpecParser) ParseComponentDataElemenSpec(specLine s
 	spec = NewComponentDataElementSpec(int32(num), isMandatory)
 	return
 }
-
-/* Parse spec for a single composite data element of the form
-
-020    C138 PRICE MULTIPLIER INFORMATION               C    1
-       5394  Price multiplier rate                     M      n..12
-       5393  Price multiplier type code qualifier      C      an..3
-
-*/ /*
-func (p *CompositeDataElementSpecParser) ParseSpec(specLines []string) (spec *CompositeDataElementSpec, err error) {
-	if len(specLines) == 0 {
-		return nil, errors.New("Missing composite spec header")
-	}
-
-	_, name, title, isMandatory, count, err := p.ParseHeader(specLines[0])
-	if err != nil {
-		return
-	}
-
-	componentSpecs := []*ComponentDataElementSpec{}
-	numSpecLines := len(specLines)
-	for i := 1; i < numSpecLines; i++ {
-		line := specLines[i]
-		if len(strings.TrimSpace(line)) == 0 {
-			continue
-		}
-		elemSpec, err := p.ParseComponentDataElemenSpec(line)
-		if err != nil {
-			return nil, err
-		}
-		componentSpecs = append(componentSpecs, elemSpec)
-	}
-
-	if len(componentSpecs) == 0 {
-		return nil, errors.New("No component elements")
-	}
-
-	spec = NewCompositeDataElementSpec(name, title, count, isMandatory, componentSpecs)
-	return
-}*/
 
 func (p *CompositeDataElementSpecParser) ParseHeader(header string) (id string, name string, err error) {
 	headerMatch := p.headerRE.FindStringSubmatch(header)
@@ -324,8 +245,6 @@ func NewCompositeDataElementSpecParser() *CompositeDataElementSpecParser {
 	// TODO account for change indicators?
 	return &CompositeDataElementSpecParser{
 		// We ignore the repr spec (already available via simple data element specs)
-		// TODO: move header RE to segment parser
-		//headerRE:        regexp.MustCompile(`^(\d{3})[ ]{4}(C\d{3}) ([A-Z ]{42}) ([CM])[ ]{4}(\d+)`),
 
 		headerRE: regexp.MustCompile(`^[ ]{7}(C[0-9]{3}) ([A-Z/& -]+) *$`),
 		// OBSOLETE componentElemRE: regexp.MustCompile(`^[ ]{7}(\d{4})  ([A-Za-z ]{41}) ([CM]) .+$`),
--- edifact/segment/specparser.go
+++ edifact/segment/specparser.go
@@ -1,21 +1,66 @@
 package segment
 
 import (
+	"errors"
+	"fmt"
 	"github.com/bbiskup/edify/edifact/util"
 	"log"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 // Parses segment specifications file (e.g. EDSD.14B)
 type SegmentSpecParser struct {
+	compositeElemRE *regexp.Regexp
 }
 
 type SegmentSpecMap map[string]*SegmentSpec
+
+// Parse composite element spec of the form
+// "020    C138 PRICE MULTIPLIER INFORMATION               C    1"
+func (p *SegmentSpecParser) ParseCompositeElemSpec(specStr string) (pos int, name string, title string, mandatory bool, count int, err error) {
+	compositeMatch := p.compositeElemRE.FindStringSubmatch(specStr)
+	if compositeMatch == nil {
+		err = errors.New(fmt.Sprintf("Unable to parse composite spec (specStr: '%s')", specStr))
+		return
+	}
+
+	if len(compositeMatch) != 6 {
+		panic("Internal error: incorrect regular expression")
+	}
+
+	pos, err = strconv.Atoi(compositeMatch[1])
+	if err != nil {
+		return
+	}
+
+	name = compositeMatch[2]
+	title = strings.TrimSpace(compositeMatch[3])
+	mandatoryStr := compositeMatch[4]
+
+	if mandatoryStr == "M" {
+		mandatory = true
+	} else if mandatoryStr == "C" {
+		mandatory = false
+	} else {
+		err = errors.New("Neither mandatory nor conditional")
+		return
+	}
+
+	count, err = strconv.Atoi(compositeMatch[5])
+	if err != nil {
+		return
+	}
+
+	return
+}
 
 // Parse a single segment specification
 func (p *SegmentSpecParser) ParseSpec(specLines []string) (spec *SegmentSpec, err error) {
 	/*for _, line := range specLines {
 
-			}*/
+																				}*/
 	panic("Not implemented")
 }
 
@@ -59,5 +104,7 @@ func (p *SegmentSpecParser) ParseSpecFile(fileName string) (specs SegmentSpecMap
 }
 
 func NewSegmentSpecParser() *SegmentSpecParser {
-	return &SegmentSpecParser{}
+	return &SegmentSpecParser{
+		compositeElemRE: regexp.MustCompile(`^(\d{3})[ ]{4}(C\d{3}) ([A-Z ]{42}) ([CM])[ ]{4}(\d+)`),
+	}
 }
