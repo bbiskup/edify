c45b106aa3787dd620d3a1532ce6c3d003015e9c
Bernhard Biskup <bbiskup@gmx.de>
2015-04-02 09:23:04 +0200

    plit out test fixture

--- edifact/dataelement/compositedataelementspecparser_test.go
+++ edifact/dataelement/compositedataelementspecparser_test.go
@@ -5,19 +5,6 @@ import (
 	"testing"
 )
 
-func fixtureMultiSimpleDataElementSpecs() SimpleDataElementSpecMap {
-	s_8179, _ := NewSimpleDataElementSpec(8179, "name_1", "descr_1", NewRepr(AlphaNum, true, 3), nil)
-	s_1131, _ := NewSimpleDataElementSpec(1131, "name_1", "descr_1", NewRepr(AlphaNum, true, 3), nil)
-	s_3055, _ := NewSimpleDataElementSpec(3055, "name_1", "descr_1", NewRepr(AlphaNum, true, 3), nil)
-	s_8178, _ := NewSimpleDataElementSpec(8178, "name_1", "descr_1", NewRepr(AlphaNum, true, 3), nil)
-	return SimpleDataElementSpecMap{
-		8179: s_8179,
-		1131: s_1131,
-		3055: s_3055,
-		8178: s_8178,
-	}
-}
-
 var parserSpec = []struct {
 	specLines      []string
 	expectedResStr string
