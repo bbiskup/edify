4f9939055d4661dcb79d70be87245a1e59fbaf3f
Bernhard Biskup <bbiskup@gmx.de>
2015-04-02 18:53:52 +0200

    Call FullSpecParser.Parse

--- edifact/fullspecparser_test.go
+++ edifact/fullspecparser_test.go
@@ -9,7 +9,12 @@ func TestFullSpecParser(t *testing.T) {
 	// TODO provide permanent test data
 	p, err := NewFullSpecParser("14B", "../testdata/d14b")
 	if err != nil {
-		t.Errorf("FullSpecParser failed: %s", err)
+		t.Errorf("NewFullSpecParser failed: %s", err)
+	}
+
+	err = p.Parse()
+	if err != nil {
+		t.Errorf("parse failed: %s", err)
 	}
 	fmt.Printf("p: %#v", p)
 }
