2689d360b37d02f2a5a47a7f2a1f20b426389aa1
Bernhard Biskup <bbiskup@gmx.de>
2015-04-02 08:58:20 +0200

    Re-enabled parse command for code lists

--- commands/parse.go
+++ commands/parse.go
@@ -1,53 +1,52 @@
 package commands
 
-/*import (
+import (
 	"errors"
 	"fmt"
 	"github.com/bbiskup/edify/edifact/codes"
-	"github.com/bbiskup/edify/edifact/dataelement"
+	// "github.com/bbiskup/edify/edifact/dataelement"
 	"log"
 	"os"
 	"strings"
-)*/
+)
 
 func Parse(fileNames []string) error {
-	panic("currently disabled; need to reorganize version handling")
-	/*for _, fileName := range fileNames {
+	for _, fileName := range fileNames {
 		err := ParseFile(fileName)
 		if err != nil {
 			return err
 		}
 	}
-	return nil*/
+	return nil
 }
 
-// func ParseFile(fileName string) error {
-// 	if len(fileName) == 0 {
-// 		return errors.New("No filename given")
-// 	}
-// 	_, err := os.Stat(fileName)
-// 	if err != nil {
-// 		return err
-// 	}
+func ParseFile(fileName string) error {
+	if len(fileName) == 0 {
+		return errors.New("No filename given")
+	}
+	_, err := os.Stat(fileName)
+	if err != nil {
+		return err
+	}
 
-// 	pathParts := strings.Split(fileName, string(os.PathSeparator))
-// 	filePart := pathParts[len(pathParts)-1]
+	pathParts := strings.Split(fileName, string(os.PathSeparator))
+	filePart := pathParts[len(pathParts)-1]
 
-// 	if strings.HasPrefix(filePart, "EDED") {
-// 		return ParseSimpleDataElements(fileName)
-// 	}
+	/*if strings.HasPrefix(filePart, "EDED") {
+		return ParseSimpleDataElements(fileName)
+	}
 
-// 	if strings.HasPrefix(filePart, "EDCD") {
-// 		return ParseCompositeDataElements(fileName)
-// 	}
+	if strings.HasPrefix(filePart, "EDCD") {
+		return ParseCompositeDataElements(fileName)
+	}*/
 
-// 	if strings.HasPrefix(filePart, "UNCL") {
-// 		spec, err := ParseCodeList(fileName)
-// 		return err
-// 	}
+	if strings.HasPrefix(filePart, "UNCL") {
+		_, err := ParseCodeList(fileName)
+		return err
+	}
 
-// 	return errors.New(fmt.Sprintf("Unrecognized file: %s", fileName))
-// }
+	return errors.New(fmt.Sprintf("Unrecognized file: %s", fileName))
+}
 
 // func ParseSimpleDataElements(fileName string) error {
 // 	log.Printf("ParseSimpleDataElements %s\n", fileName)
@@ -57,10 +56,6 @@ func Parse(fileNames []string) error {
 // 		return err
 // 	}
 // 	fmt.Printf("Found %d specs", len(specs))
-// 	/*fmt.Printf("Specs:\n")
-// 	for _, spec := range specs {
-// 		fmt.Printf("\t%s\n", spec)
-// 	}*/
 // 	fmt.Println("")
 // 	return nil
 // }
@@ -77,18 +72,18 @@ func Parse(fileNames []string) error {
 // 	return nil
 // }
 
-// func ParseCodeList(fileName string) (CodesSpecMap, error) {
-// 	log.Printf("ParseCodeList %s\n", fileName)
-// 	p := codes.NewCodesSpecParser()
-// 	specs, err := p.ParseSpecFile(fileName)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	fmt.Printf("Found %d specs", len(specs))
-// 	/*fmt.Printf("Specs:\n")
-// 	for _, spec := range specs {
-// 		fmt.Printf("\t%s\n", spec)
-// 	}*/
-// 	fmt.Println("")
-// 	return nil
-// }
+func ParseCodeList(fileName string) (codes.CodesSpecMap, error) {
+	log.Printf("ParseCodeList %s\n", fileName)
+	p := codes.NewCodesSpecParser()
+	specs, err := p.ParseSpecFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("Found %d specs", len(specs))
+	/*fmt.Printf("Specs:\n")
+	for _, spec := range specs {
+		fmt.Printf("\t%s\n", spec)
+	}*/
+	fmt.Println("")
+	return specs, nil
+}
