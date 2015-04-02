cc0588293c9c40617672ebea28177e021b06afaf
Bernhard Biskup <bbiskup@gmx.de>
2015-04-02 15:45:58 +0200

    New fields for SegmentSpec

--- edifact/segment/segmentspec.go
+++ edifact/segment/segmentspec.go
@@ -2,17 +2,28 @@ package segment
 
 import (
 	"fmt"
+	"github.com/bbiskup/edify/edifact/dataelement"
 )
 
 // Segment specification
 type SegmentSpec struct {
-	Name                string
-	Function            string
-	DataElementSpecNums []int32
+	Id               string
+	Name             string
+	Function         string
+	DataElementSpecs []*dataelement.DataElementSpec
 }
 
 func (s *SegmentSpec) String() string {
 	return fmt.Sprintf(
-		"Segment %s (%d data elems)",
-		s.Name, len(s.DataElementSpecNums))
+		"Segment %s/%s (%d data elems)",
+		s.Id, s.Name, len(s.DataElementSpecs))
+}
+
+func NewSegmentSpec(id string, name string, function string, dataElementSpecs []*dataelement.DataElementSpec) *SegmentSpec {
+	return &SegmentSpec{
+		Id:               id,
+		Name:             name,
+		Function:         function,
+		DataElementSpecs: dataElementSpecs,
+	}
 }
