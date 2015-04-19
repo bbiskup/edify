5580f54bf643b8da747124b096e24be060cb4805
Bernhard Biskup <bbiskup@gmx.de>
2015-04-19 10:04:06 +0200

    Added function purgeDownloads

--- commands/purge_specs.go
+++ commands/purge_specs.go
@@ -2,16 +2,51 @@ package commands
 
 import (
 	"errors"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
-// Purge extracted EDIFACT spec content
-func PurgeSpecs(version string) error {
+const pathSep = string(os.PathSeparator)
+
+// Delete all .zip files in download dir
+func purgeDownloads() error {
+	entries, err := ioutil.ReadDir(downloadDir)
+	if err != nil {
+		return err
+	}
+	log.Printf("Purging %d downloaded archives in %s", len(entries), downloadDir)
+
+	for _, entry := range entries {
+		fileName := entry.Name()
+		if !strings.HasSuffix(fileName, ".zip") {
+			log.Printf("Skipping %s", fileName)
+			continue
+		}
+		log.Printf("Deleting %s", fileName)
+		err := os.Remove(downloadDir + pathSep + fileName)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Purge extracted EDIFACT spec content, and possibly
+// download archives if purgeAll == true
+func PurgeSpecs(version string, purgeAll bool) error {
+	if purgeAll {
+		err := purgeDownloads()
+		if err != nil {
+			return err
+		}
+	}
+
 	if len(version) == 0 {
 		return errors.New("No version specified")
 	}
-	versionSubDir := downloadDir + string(os.PathSeparator) + versionDir(version)
+	versionSubDir := downloadDir + pathSep + versionDir(version)
 	s, err := os.Stat(versionSubDir)
 	if !os.IsNotExist(err) && s.IsDir() {
 		log.Printf("Purging version %s (%s)", version, versionSubDir)
