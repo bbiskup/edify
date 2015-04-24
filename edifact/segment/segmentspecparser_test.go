5331c97b8cd947d6e3242ff45f333af468b6070e
Bernhard Biskup <bbiskup@gmx.de>
2015-04-24 08:39:14 +0200

    New test: parsing single segment spec

--- edifact/segment/segmentspecparser_test.go
+++ edifact/segment/segmentspecparser_test.go
@@ -2,12 +2,9 @@ package segment
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
-
-func TestParseSpec(t *testing.T) {
-	_ = NewSegmentSpecParser(nil, nil)
-}
 
 const dataElemSpecStr = "010    C817 ADDRESS USAGE                              C    1"
 
@@ -36,5 +33,48 @@ func TestParseDataElemSpec(t *testing.T) {
 
 	if dataElementKind != Composite {
 		t.Errorf("Should be composite")
+	}
+}
+
+const segmentSpec = `
+       CDI  PHYSICAL OR LOGICAL STATE
+
+       Function: To describe a physical or logical state.
+
+010    7001 PHYSICAL OR LOGICAL STATE TYPE CODE
+            QUALIFIER                                  M    1 an..3
+
+020    C564 PHYSICAL OR LOGICAL STATE INFORMATION      M    1
+       7007  Physical or logical state description
+             code                                      C      an..3
+       1131  Code list identification code             C      an..17
+       3055  Code list responsible agency code         C      an..3
+       7006  Physical or logical state description     C      an..70
+`
+
+func TestParseSpec(t *testing.T) {
+	p := NewSegmentSpecParser(nil, nil)
+	specLines := strings.Split(segmentSpec, "\n")
+	segmentSpec, err := p.ParseSpec(specLines)
+
+	if err != nil {
+		t.Fatalf("Parsing failed: %s", err)
+	}
+
+	if segmentSpec.Id != "CDI" {
+		t.Errorf("Id should be CDI; was %s", segmentSpec.Id)
+	}
+
+	if segmentSpec.Name != "PHYSICAL OR LOGICAL STATE" {
+		t.Errorf("Name should be 'PHYSICAL OR LOGICAL STATE'; was %s", segmentSpec.Name)
+	}
+
+	if segmentSpec.Function != "To describe a physical or logical state." {
+		t.Errorf("Name should be 'To describe a physical or logical state.'; was %s", segmentSpec.Name)
+	}
+
+	lenSegmentDataElementSpecs := len(segmentSpec.SegmentDataElementSpecs)
+	if lenSegmentDataElementSpecs != 2 {
+		t.Errorf("Expected 2 data element specs; got %d", lenSegmentDataElementSpecs)
 	}
 }
