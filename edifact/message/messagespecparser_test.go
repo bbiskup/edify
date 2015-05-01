e115f2775a5ea76f4ed25fb17eeab8f0b9656a80
Bernhard Biskup <bbiskup@gmx.de>
2015-05-01 21:52:26 +0200

    Added test case for passing AUTHOR message spec

--- edifact/message/messagespecparser_test.go
+++ edifact/message/messagespecparser_test.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-func TestParseFile(t *testing.T) {
+func TestParseINVOICFile(t *testing.T) {
 	segmentSpecs := segment.SegmentSpecMap{} // TODO actual fixture
 	parser := NewMessageSpecParser(segmentSpecs)
 	spec, err := parser.ParseSpecFile("../../testdata/INVOIC_D.14B")
@@ -21,5 +21,21 @@ func TestParseFile(t *testing.T) {
 	assert.Equal(t, "14B", spec.Release)
 	assert.Equal(t, "UN", spec.ContrAgency)
 	assert.Equal(t, "16", spec.Revision)
+	assert.Equal(t, time.Date(2014, time.November, 17, 0, 0, 0, 0, time.UTC), spec.Date)
+}
+
+func TestParseAUTHORFile(t *testing.T) {
+	segmentSpecs := segment.SegmentSpecMap{} // TODO actual fixture
+	parser := NewMessageSpecParser(segmentSpecs)
+	spec, err := parser.ParseSpecFile("../../testdata/AUTHOR_D.14B")
+	assert.Nil(t, err)
+	assert.NotNil(t, spec)
+	fmt.Printf("Message spec: %s", spec)
+
+	assert.Equal(t, "AUTHOR", spec.Id)
+	assert.Equal(t, "D", spec.Version)
+	assert.Equal(t, "14B", spec.Release)
+	assert.Equal(t, "UN", spec.ContrAgency)
+	assert.Equal(t, "3", spec.Revision)
 	assert.Equal(t, time.Date(2014, time.November, 17, 0, 0, 0, 0, time.UTC), spec.Date)
 }
