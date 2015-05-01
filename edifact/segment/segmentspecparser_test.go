0f442b9b21b7372e39af20dfd9d7e90c950c77b2
Bernhard Biskup <bbiskup@gmx.de>
2015-05-01 17:21:04 +0200

    Use testify assertions

--- edifact/segment/segmentspecparser_test.go
+++ edifact/segment/segmentspecparser_test.go
@@ -1,7 +1,6 @@
 package segment
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"strings"
 	"testing"
@@ -13,10 +12,7 @@ func TestParseDataElemSpec(t *testing.T) {
 	p := NewSegmentSpecParser(nil, nil)
 
 	pos, id, dataElementKind, count, isMandatory, err := p.parseDataElemSpec(dataElemSpecStr)
-	if err != nil {
-		t.Fatalf(fmt.Sprintf("Error parsing data elem spec: %s", err))
-	}
-
+	assert.Nil(t, err, "Unable to parse DataElementSpec")
 	assert.Equal(t, 10, pos, "Incorrect position")
 	assert.Equal(t, "C817", id, "Incorrect ID")
 	assert.Equal(t, 1, count, "Incorrect count")
@@ -45,24 +41,11 @@ func TestParseSpec(t *testing.T) {
 	specLines := strings.Split(segmentSpec, "\n")
 	segmentSpec, err := p.ParseSpec(specLines)
 
-	if err != nil {
-		t.Fatalf("Parsing failed: %s", err)
-	}
-
-	if segmentSpec.Id != "CDI" {
-		t.Errorf("Id should be CDI; was %s", segmentSpec.Id)
-	}
-
-	if segmentSpec.Name != "PHYSICAL OR LOGICAL STATE" {
-		t.Errorf("Name should be 'PHYSICAL OR LOGICAL STATE'; was %s", segmentSpec.Name)
-	}
-
-	if segmentSpec.Function != "To describe a physical or logical state." {
-		t.Errorf("Name should be 'To describe a physical or logical state.'; was %s", segmentSpec.Name)
-	}
+	assert.Nil(t, err, "Parsing failed")
+	assert.Equal(t, "CDI", segmentSpec.Id, "Id incorrect")
+	assert.Equal(t, "PHYSICAL OR LOGICAL STATE", segmentSpec.Name, "Incorrect Name")
+	assert.Equal(t, "To describe a physical or logical state.", segmentSpec.Function, "Incorrect Function")
 
 	lenSegmentDataElementSpecs := len(segmentSpec.SegmentDataElementSpecs)
-	if lenSegmentDataElementSpecs != 2 {
-		t.Errorf("Expected 2 data element specs; got %d", lenSegmentDataElementSpecs)
-	}
+	assert.Equal(t, 2, lenSegmentDataElementSpecs, "Incorrect len")
 }
