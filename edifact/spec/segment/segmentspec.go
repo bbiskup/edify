ec4756dea4f26cbbc1c5d6acf37b73c3879c808c
Bernhard Biskup <bbiskup@gmx.de>
2015-05-03 22:17:54 +0200

    SegmentSpecProvider/SegmentSpecProviderImpl in separate file

--- edifact/spec/segment/segmentspec.go
+++ edifact/spec/segment/segmentspec.go
@@ -2,7 +2,6 @@ package segment
 
 import (
 	"fmt"
-	"log"
 )
 
 // Segment specification
@@ -14,36 +13,6 @@ type SegmentSpec struct {
 }
 
 type SegmentSpecMap map[string]*SegmentSpec
-
-// Provides segment spec by Id
-type SegmentSpecProvider interface {
-	Get(id string) *SegmentSpec
-	Len() int
-}
-
-// Regular implementation of SegmentSpecProvider for production
-type SegmentSpecProviderImpl struct {
-	segmentSpecs SegmentSpecMap
-}
-
-func (p *SegmentSpecProviderImpl) Get(id string) *SegmentSpec {
-	result := p.segmentSpecs[id]
-	if result == nil {
-		// e.g. UNH, UNT are not defined in UNCE specs, because they
-		// are not part of the release cycle. Instead, they are defined
-		// in part 1 of ISO9735 (file testdata/r1241.txt)
-		log.Printf("######################## Missing segment spec: '%s'", id)
-		return NewSegmentSpec(
-			id, fmt.Sprintf("missing-%s", id),
-			"dummy_function", nil)
-	} else {
-		return result
-	}
-}
-
-func (p *SegmentSpecProviderImpl) Len() int {
-	return len(p.segmentSpecs)
-}
 
 func (s *SegmentSpec) String() string {
 	return fmt.Sprintf(
---
+++ edifact/spec/segment/segmentspecprovider.go
@@ -0,0 +1,36 @@
+package segment
+
+import (
+	"fmt"
+	"log"
+)
+
+// Provides segment spec by Id
+type SegmentSpecProvider interface {
+	Get(id string) *SegmentSpec
+	Len() int
+}
+
+// Regular implementation of SegmentSpecProvider for production
+type SegmentSpecProviderImpl struct {
+	segmentSpecs SegmentSpecMap
+}
+
+func (p *SegmentSpecProviderImpl) Get(id string) *SegmentSpec {
+	result := p.segmentSpecs[id]
+	if result == nil {
+		// e.g. UNH, UNT are not defined in UNCE specs, because they
+		// are not part of the release cycle. Instead, they are defined
+		// in part 1 of ISO9735 (file testdata/r1241.txt)
+		log.Printf("######################## Missing segment spec: '%s'", id)
+		return NewSegmentSpec(
+			id, fmt.Sprintf("missing-%s", id),
+			"dummy_function", nil)
+	} else {
+		return result
+	}
+}
+
+func (p *SegmentSpecProviderImpl) Len() int {
+	return len(p.segmentSpecs)
+}
