8c31bf3b8e566404d3f9029ce50e4dafdd9882f5
Bernhard Biskup <bbiskup@gmx.de>
2015-05-10 18:24:36 +0200

    New test cases

---
+++ edifact/msg/message_test.go
@@ -0,0 +1,18 @@
+package msg
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStringEmpty(t *testing.T) {
+	msg := NewMessage("ABC")
+	assert.Equal(t, "ABC\n", msg.String())
+}
+
+func TestStringWithSegments(t *testing.T) {
+	msg := NewMessage("ABC")
+	segment1 := NewSegment("XYZ")
+	msg.AddSegment(segment1)
+	assert.Equal(t, "ABC\n\tXYZ\n\n", msg.String())
+}
