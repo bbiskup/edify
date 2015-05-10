6db4e1dd5c77a006cb4e751fcaa57edfc376aadd
Bernhard Biskup <bbiskup@gmx.de>
2015-05-10 18:36:32 +0200

    Whitespace

--- edifact/msg/message_test.go
+++ edifact/msg/message_test.go
@@ -11,7 +11,6 @@ func TestStringEmpty(t *testing.T) {
 }
 
 func TestStringWithSegments(t *testing.T) {
-
 	segment1 := NewSegment("XYZ")
 	msg := NewMessage("ABC", []*Segment{segment1})
 	assert.Equal(t, 1, len(msg.Segments))
