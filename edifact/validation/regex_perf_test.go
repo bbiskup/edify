c557d8839dd26a44ca0fefb0665b55f13758bf93
Bernhard Biskup <bbiskup@gmx.de>
2015-05-10 09:17:40 +0200

    Added benchmark for matching large regexp
     -> 20 s for 1000 x 2 groups --> far too slow to be practical

--- edifact/validation/regex_perf_test.go
+++ edifact/validation/regex_perf_test.go
@@ -7,16 +7,58 @@ import (
 	"testing"
 )
 
-func BenchmarkCompileLargeRegexp(b *testing.B) {
+func createLargeRegexpStr() string {
 	var buffer bytes.Buffer
+	buffer.WriteString("^")
 	for i := 0; i < 1000; i++ {
-		buffer.WriteString("(AAA:{1,3})(BBB:{0,2})")
+		buffer.WriteString("(AAA:){0,3}(BBB:){0,2}")
 	}
-	patStr := buffer.String()
+	buffer.WriteString("$")
+	return buffer.String()
+}
+
+func BenchmarkCompileLargeRegexp(b *testing.B) {
+	patStr := createLargeRegexpStr()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		r := regexp.MustCompile(patStr)
-		assert.NotNil(b, r)
+
+		if i == 0 {
+			assert.NotNil(b, r)
+		}
+	}
+}
+
+func BenchmarkMatchLargeRegexp(b *testing.B) {
+	patStr := createLargeRegexpStr()
+	r := regexp.MustCompile(patStr)
+	assert.NotNil(b, r)
+
+	numSegments := 1000
+
+	var buffer bytes.Buffer
+	for i := 0; i < numSegments; i++ {
+		var next string
+		if i%2 == 0 {
+			next = "AAA:AAA:AAA:"
+		} else {
+			next = "BBB:BBB:"
+		}
+		buffer.WriteString(next)
+	}
+	strToMatch := buffer.String()
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		m := r.FindStringSubmatch(strToMatch)
+
+		if i == 0 {
+			assert.NotNil(b, m)
+
+			// + 1 for group 0 (full match)
+			assert.Equal(b, numSegments+1, len(m))
+		}
 	}
 }
