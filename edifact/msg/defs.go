bda12b790335256b7d69fef2034468c5771cc866
Bernhard Biskup <bbiskup@gmx.de>
2015-05-14 10:24:37 +0200

    Added definition for data element repetition character

--- edifact/msg/defs.go
+++ edifact/msg/defs.go
@@ -11,5 +11,14 @@ const (
 	CompDataElemSep    = ':'
 	CompDataElemSepStr = string(CompDataElemSep)
 
+	// Definitions
+	// A.64    repeating data element: A composite data element or stand-
+	// alone data element having a maximum occurrence of greater than
+	// one in the segment specification.  [ 64 ]
+	//
+	// A.65 repetition separator: A service character used to separate
+	// adjacent occurrences of a repeating data element.  [ 65 ]
+	RepetitionSep = "*"
+
 	ReleaseChar = '?'
 )
