ebdb76477a49e066b073efa2303a0dd954c3280e
Bernhard Biskup <bbiskup@gmx.de>
2015-05-14 12:11:32 +0200

    New test case for INVOIC

--- edifact/msg/parser_test.go
+++ edifact/msg/parser_test.go
@@ -3,6 +3,7 @@ package msg
 import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"io/ioutil"
 	"testing"
 )
 
@@ -30,6 +31,20 @@ func TestParser1(t *testing.T) {
 	assert.Equal(t, "ORDERS", message.Segments[0].Elements[1].Values[0])
 	assert.Equal(t, "D", message.Segments[0].Elements[1].Values[1])
 	assert.Equal(t, "9", message.Segments[8].Elements[0].Values[0])
+}
+
+func TestParseINVOIC(t *testing.T) {
+	msgStr, err := ioutil.ReadFile("../../testdata/messages/INVOIC_1.txt")
+	require.Nil(t, err)
+
+	p := NewParser()
+	message, err := p.ParseMessage(string(msgStr))
+	require.Nil(t, err)
+	require.NotNil(t, message)
+
+	assert.Equal(t, "UNH", message.Segments[0].Id)
+	assert.Equal(t, "UNT", message.Segments[112].Id)
+	//assert.Equal(t, "xxx", message.Segments[4].Elements[3].Values[0])
 }
 
 var elemSpecs = []struct {
