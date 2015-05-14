7c53bd280905c09888ecbe55b92f08e531f6c86e
Bernhard Biskup <bbiskup@gmx.de>
2015-05-14 11:55:02 +0200

    Comments

--- edifact/msg/defs.go
+++ edifact/msg/defs.go
@@ -1,10 +1,15 @@
 package msg
 
+// Special segment identifiers
+const (
+	UNH = "UNH"
+	UNT = "UNT"
+)
+
+// Service characters
 // http://www.unece.org/trade/untdid/texts/d423.htm
 // Chapter 2.2 "Syntax separators, terminator and release character"
 const (
-	UNH               = "UNH"
-	UNT               = "UNT"
 	SegTerm           = '\''
 	SegTagDataElemSep = '+'
 
