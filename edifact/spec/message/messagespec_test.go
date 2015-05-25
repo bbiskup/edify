a4728d68118083431b67d8ce00a62e62824b2e11
Bernhard Biskup <bbiskup@gmx.de>
2015-05-25 12:29:13 +0200

    Refactored test case as tabular

--- edifact/spec/message/messagespec_test.go
+++ edifact/spec/message/messagespec_test.go
@@ -6,15 +6,27 @@ import (
 	"time"
 )
 
-func TestString(t *testing.T) {
-	s := NewMessageSpec(
-		"testid", "testname",
-		"testversion", "testrelease",
-		"testcontragency", "testrevision",
-		time.Date(2015, time.January, 15, 0, 0, 0, 0, time.UTC),
-		"testsource",
-		[]MessageSpecPart{},
-	)
+var partsSpec = []struct {
+	parts    []MessageSpecPart
+	expected string
+}{
+	{
+		parts:    []MessageSpecPart{},
+		expected: "Message testid (testname testrelease): 0 parts",
+	},
+}
 
-	assert.Equal(t, s.String(), "Message testid (testname testrelease): 0 parts")
+func TestString(t *testing.T) {
+	for _, spec := range partsSpec {
+		s := NewMessageSpec(
+			"testid", "testname",
+			"testversion", "testrelease",
+			"testcontragency", "testrevision",
+			time.Date(2015, time.January, 15, 0, 0, 0, 0, time.UTC),
+			"testsource",
+			[]MessageSpecPart{},
+		)
+
+		assert.Equal(t, spec.expected, s.String())
+	}
 }
