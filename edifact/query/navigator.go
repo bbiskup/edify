e2156349602b49ede132a2de18c548cba00c489e
Bernhard Biskup <bbiskup@gmx.de>
2015-05-30 11:18:09 +0200

    Fragment of message navigator

---
+++ edifact/query/navigator.go
@@ -0,0 +1,57 @@
+package query
+
+// import (
+// 	"errors"
+// 	"fmt"
+// 	"github.com/bbiskup/edify/edifact/msg"
+// )
+
+// // Provides navigation within EDIFACT message
+// type Navigator struct {
+// }
+
+// func (n *Navigator) navigate(queryStr string, message *msg.NestedMessage) (msgPart msg.NestedMsgPart, err error) {
+// 	// return segment, segment group or data element
+// 	panic("Not implemented")
+// }
+
+// func (n *Navigator) GetSegmentGroup(
+// 	queryStr string, message *msg.NestedMessage) (group *msg.SegmentGroup, err error) {
+// 	msgPart, err := n.navigate(queryStr, message)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	group, ok := msgPart.(*msg.SegmentGroup)
+// 	if !ok {
+// 		return nil, errors.New(fmt.Sprintf("Unexpected type %T", group))
+// 	}
+// 	return group, nil
+// }
+
+// func (n *Navigator) GetSegment(queryStr string, message *msg.NestedMessage) (*msg.Segment, error) {
+// 	msgPart, err := n.navigate(queryStr, message)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	segment, ok := msgPart.(*msg.Segment)
+// 	if !ok {
+// 		return nil, errors.New(fmt.Sprintf("Unexpected type %T", segment))
+// 	}
+// 	return segment, nil
+// }
+
+// func (n *Navigator) GetSegmentDataElem(queryStr string, message *msg.NestedMessage) (*msg.DataElement, error) {
+// 	msgPart, err := n.navigate(queryStr, message)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	dataElem, ok := msgPart.(*msg.DataElement)
+// 	if !ok {
+// 		return nil, errors.New(fmt.Sprintf("Unexpected type %T", dataElem))
+// 	}
+// 	return dataElem, nil
+// }
+
+// func NewNavigator() *Navigator {
+// 	return &Navigator{}
+// }
