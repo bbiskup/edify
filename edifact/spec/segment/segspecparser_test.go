351c02aa21af939ec93a6381ff8972dbcbd0a992
Bernhard Biskup <bbiskup@gmx.de>
2015-06-07 10:49:32 +0200

    Added test case

--- edifact/spec/segment/segspecparser_test.go
+++ edifact/spec/segment/segspecparser_test.go
@@ -49,3 +49,37 @@ func TestParseSpec(t *testing.T) {
 	lenSegDataElemSpecs := len(segSpec.SegDataElemSpecs)
 	assert.Equal(t, 2, lenSegDataElemSpecs)
 }
+
+const segSpecWithNote = `
+       ARR  ARRAY INFORMATION
+
+       Function: To contain the data in an array.
+
+010    C778 POSITION IDENTIFICATION                    C    1
+       7164  Hierarchical structure level identifier   C      an..35
+       1050  Sequence position identifier              C      an..10
+
+020    C770 ARRAY CELL DETAILS                         C    1
+       9424  Array cell data description               C      an..512
+
+       Note: 
+            The composite C770 - array cell details - occurs
+            9,999 times in the segment. The use of the ARR
+            segment is restricted to be used only with Version 3
+            of ISO-9735.
+            The component 9424 - array cell information - occurs
+            100 times in the composite C770. The use of C770 is
+            restricted to be used only with the ARR segment
+            within Version 3 of ISO-9735.
+`
+
+func TestParseSpecWithNode(t *testing.T) {
+	p := NewSegSpecParser(nil, nil)
+	specLines := strings.Split(segSpecWithNote, "\n")
+	segSpec, err := p.ParseSpec(specLines)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "ARR", segSpec.Id)
+	lenSegDataElemSpecs := len(segSpec.SegDataElemSpecs)
+	assert.Equal(t, 2, lenSegDataElemSpecs)
+}
