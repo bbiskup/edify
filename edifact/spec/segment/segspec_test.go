5cf7f9489d6907c930d7b4866a09c31b26513c48
Bernhard Biskup <bbiskup@gmx.de>
2015-06-07 10:46:23 +0200

    Added test case

--- edifact/spec/segment/segspec_test.go
+++ edifact/spec/segment/segspec_test.go
@@ -1,7 +1,9 @@
 package segment
 
 import (
+	dsp "github.com/bbiskup/edify/edifact/spec/dataelement"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"strings"
 	"testing"
 )
@@ -33,4 +35,20 @@ func TestParseFunction(t *testing.T) {
 	fun, err := parser.parseFunction(funcLines)
 	assert.Nil(t, err)
 	assert.Equal(t, expectedFun, fun)
+}
+
+func TestNumLeadingMandDataElems(t *testing.T) {
+	sd1, err := dsp.NewSimpleDataElemSpec(
+		"d1", "d1_name", "d1_descr",
+		dsp.NewRepr(dsp.AlphaNum, true, 3), nil)
+	require.Nil(t, err)
+
+	sd2, err := dsp.NewSimpleDataElemSpec(
+		"d1", "d1_name", "d1_descr",
+		dsp.NewRepr(dsp.AlphaNum, false, 3), nil)
+	require.Nil(t, err)
+
+	spec := NewSegSpec("ABC", "ABC_name", "ABC_descr",
+		[]*SegDataElemSpec{NewSegDataElemSpec(sd1, 1, true), NewSegDataElemSpec(sd2, 1, false)})
+	assert.Equal(t, 1, spec.NumLeadingMandDataElems())
 }
