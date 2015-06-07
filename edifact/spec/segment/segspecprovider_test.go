776b39553fa88f33192a27fb2818a0c4ba2ce5b0
Bernhard Biskup <bbiskup@gmx.de>
2015-06-07 10:28:25 +0200

    New tests

--- edifact/spec/segment/segspecprovider_test.go
+++ edifact/spec/segment/segspecprovider_test.go
@@ -2,6 +2,7 @@ package segment
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -12,4 +13,16 @@ func TestIsUnValidatedSegment(t *testing.T) {
 	assert.Equal(t, true, IsUnValidatedSegment("UNS"))
 	assert.Equal(t, true, IsUnValidatedSegment("UGH"))
 	assert.Equal(t, true, IsUnValidatedSegment("UGT"))
+}
+
+func TestSegSpecProvider(t *testing.T) {
+	segSpecs := SegSpecMap{
+		"ADR": NewSegSpec("ADR", "adr_name", "adr_func", nil),
+	}
+	prov := NewSegSpecProviderImpl(segSpecs)
+	assert.Equal(t, 1, prov.Len())
+
+	adr := prov.Get("ADR")
+	require.NotNil(t, adr)
+	assert.Equal(t, "ADR", adr.Id)
 }
