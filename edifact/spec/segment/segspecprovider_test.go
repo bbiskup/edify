9305b2baf6d6fd73c414b1fddb49b9f301d79bc4
Bernhard Biskup <bbiskup@gmx.de>
2015-06-07 10:35:13 +0200

    New test case

--- edifact/spec/segment/segspecprovider_test.go
+++ edifact/spec/segment/segspecprovider_test.go
@@ -15,14 +15,25 @@ func TestIsUnValidatedSegment(t *testing.T) {
 	assert.Equal(t, true, IsUnValidatedSegment("UGT"))
 }
 
-func TestSegSpecProvider(t *testing.T) {
+func getSegSpecProviderImpl(t *testing.T) *SegSpecProviderImpl {
 	segSpecs := SegSpecMap{
 		"ADR": NewSegSpec("ADR", "adr_name", "adr_func", nil),
 	}
 	prov := NewSegSpecProviderImpl(segSpecs)
-	assert.Equal(t, 1, prov.Len())
+	return prov
+}
 
+func TestSegSpecProviderGetLen(t *testing.T) {
+	prov := getSegSpecProviderImpl(t)
+	assert.Equal(t, 1, prov.Len())
 	adr := prov.Get("ADR")
 	require.NotNil(t, adr)
 	assert.Equal(t, "ADR", adr.Id)
+}
+
+func TestSegSpecProviderGetUnValidated(t *testing.T) {
+	prov := getSegSpecProviderImpl(t)
+	adr := prov.Get("UNH")
+	require.NotNil(t, adr)
+	assert.Equal(t, "UNH", adr.Id)
 }
